08496ee278b2e607106e99d97daa0cd4005f01ea
Rob Mayoff <git@rob.dqd.com>
2012-12-07 22:51:53 -0600

    remove boilerplate from main.m

--- mickeyMouse/main.m
+++ mickeyMouse/main.m
@@ -10,12 +10,7 @@
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
     }
     return 0;
 }
