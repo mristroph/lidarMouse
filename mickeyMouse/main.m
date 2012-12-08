762251a92d8066d6571dfa356243f3bc85bf077b
Rob Mayoff <git@rob.dqd.com>
2012-12-07 22:35:13 -0600

    Initial Commit

---
+++ mickeyMouse/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mickeyMouse
+//
+//  Created by Rob Mayoff on 12/7/12.
+//  Copyright (c) 2012 Rob Mayoff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
