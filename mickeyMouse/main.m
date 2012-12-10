795c180c6ebe463e6fa61258301887aeee524aab
Rob Mayoff <git@rob.dqd.com>
2012-12-10 16:56:59 -0600

    stop using liburg

--- mickeyMouse/main.m
+++ mickeyMouse/main.m
@@ -17,30 +17,9 @@ int main(int argc, const char * argv[]) {
     (void)argc; (void)argv;
 
     @autoreleasepool {
-
         Application *application = [[Application alloc] init];
         (void)application;
         CFRunLoopRun();
-        return 0;
-
-        urg_t urg;
-
-        if (open_urg_sensor(&urg, 0, NULL) < 0) {
-            fprintf(stderr, "error: open_urg_sensor failed: %s (%d)", strerror(errno), errno);
-            return 1;
-        }
-
-        long data[urg_max_data_size(&urg)];
-        urg_start_measurement(&urg, URG_DISTANCE, 1, 0);
-
-        long timestamp;
-        int n = urg_get_distance(&urg, data, &timestamp);
-
-        for (int i = 0; i < n; ++i) {
-            printf("%d: %ld\n", i, data[i]);
-        }
-
-        urg_close(&urg);
         return 0;
     }
 }
