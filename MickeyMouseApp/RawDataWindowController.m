8bfb66ba366989d335f3c2838d501eb9c3909c7f
Rob Mayoff <git@rob.dqd.com>
2012-12-12 01:20:23 -0600

    do untouched calibration in window controller

--- MickeyMouseApp/RawDataWindowController.m
+++ MickeyMouseApp/RawDataWindowController.m
@@ -11,6 +11,9 @@
 #import "MyWindow.h"
 #import "Lidar2D.h"
 
+// This should really be read from the Lidar2D object.  Hardcoding for convenience.  Fix later.  XXX
+#define kRayCount (725U-44U+1U)
+
 @interface RawDataWindowController ()
 
 @end
@@ -20,8 +23,11 @@
     id<Lidar2DProxy> proxy_;
     id windowWillOrderInObserver_;
     id windowWillCloseObserver_;
+    IBOutlet NSTextField *statusField_;
     IBOutlet RawDataGraphView *graphView_;
     volatile BOOL wantsStreamingData_;
+
+    uint32_t untouchedRayDistances[kRayCount];
 }
 
 #pragma mark - Public API
@@ -42,6 +48,7 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
+    statusField_.stringValue = @"Loading";
     [self startObservingWindowPresence];
 }
 
@@ -84,32 +91,85 @@
     wantsStreamingData_ = YES;
 
     [proxy_ performBlock:^(id<Lidar2D> device) {
-        if (device.error) {
-            NSLog(@"device error: %@", device.error);
-            device.error = nil;
-        }
+        [self updateStatusLabelIfNecessaryAndClearErrorWithDevice:device];
 
+        [self setStatusLabelText:@"Preparing to calibrate - REMOVE ALL OBSTRUCTIONS FROM SCREEN"];
+        [self resetUntouchedRayDistances];
+        sleep(1);
+        [self setStatusLabelText:@"Calibrating - DO NOT OBSTRUCT SCREEN"];
+        __block int calibrationFramesLeft = 20;
+        [device forEachStreamingDataSnapshot:^(NSData *data, BOOL *stop) {
+            [self setStatusLabelText:[NSString stringWithFormat:@"Calibrating %d - DO NOT OBSTRUCT SCREEN", calibrationFramesLeft]];
+            [self updateUntouchedRayDistancesWithDevice:device data:data];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                graphView_.data = data;
+            });
+            *stop = (--calibrationFramesLeft < 1);
+        }];
+
+        [self setStatusLabelText:@"Preparing to calibrate - REMOVE ALL OBSTRUCTIONS FROM SCREEN"];
         while (wantsStreamingData_) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                statusField_.stringValue = @"Requesting streaming data";
+            });
             [device forEachStreamingDataSnapshot:^(NSData *data, BOOL *stop) {
                 dispatch_async(dispatch_get_main_queue(), ^{
+                    statusField_.stringValue = @"Received streaming data";
                     graphView_.data = data;
                 });
                 *stop = !wantsStreamingData_;
             }];
 
-            if (device.error) {
-                NSLog(@"device error: %@", device.error);
-                device.error = nil;
+            NSError *error = device.error;
+            [self updateStatusLabelIfNecessaryAndClearErrorWithDevice:device];
+            if (error) {
                 if (wantsStreamingData_) {
                     sleep(1);
                 }
+            } else {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    statusField_.stringValue = @"Streaming data stopped";
+                });
             }
         }
     }];
 }
 
+- (void)setStatusLabelText:(NSString *)text {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        statusField_.stringValue = text;
+    });
+}
+
+- (void)updateStatusLabelIfNecessaryAndClearErrorWithDevice:(id<Lidar2D>) device {
+    NSError *error = device.error;
+    if (error) {
+        [self setStatusLabelText:@"Device Error"];
+        NSLog(@"device error: %@", device.error);
+        device.error = nil;
+    }
+}
+
 - (void)stopStreamingData {
     wantsStreamingData_ = NO;
+}
+
+- (void)resetUntouchedRayDistances {
+    for (size_t i = 0; i < kRayCount; ++i) {
+        untouchedRayDistances[i] = UINT32_MAX;
+    }
+}
+
+- (void)updateUntouchedRayDistancesWithDevice:(id<Lidar2D>)device data:(NSData *)data {
+    (void)device; // only passed to ensure I'm run on the device's queue
+    uint32_t const *distances = (uint32_t const *)data.bytes;
+    size_t count = MIN(data.length / sizeof *distances, kRayCount);
+    for (size_t i = 0; i < count; ++i) {
+        uint32_t distance = distances[i];
+        if (distance >= 20 && distance < untouchedRayDistances[i]) {
+            untouchedRayDistances[i] = distance;
+        }
+    }
 }
 
 @end
