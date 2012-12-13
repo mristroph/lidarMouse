a6adc1a2ce19b7d324a6a4d6ef19c0ec76d5c145
Rob Mayoff <git@rob.dqd.com>
2012-12-13 17:51:22 -0600

    revamp Lidar2D interface - implementation not changed yet so build is broken

--- Lidar2D/Lidar2D.h
+++ Lidar2D/Lidar2D.h
@@ -5,37 +5,70 @@ Copyright (c) 2012 Rob Mayoff. All rights reserved.
 
 #import <Foundation/Foundation.h>
 
-@protocol Lidar2D <NSObject>
+@protocol Lidar2DObserver;
+
+typedef uint32_t Lidar2DDistance;
+
+@interface Lidar2D : NSObject
+
+// I initialize myself to connect to the device at `path`.  I don't actually open the device until you send me `connect`.
+- (id)initWithDevicePath:(NSString *)path;
 
 @property (nonatomic, readonly) NSString *devicePath;
 
-// This is an error I have encountered, or nil.  Set this to nil after handling the error if you want to try to keep using me.
-@property (nonatomic, strong) NSError *error;
+// I start connecting to the device on a background thread.  I notify my observers when I have finished connecting.
+- (void)connect;
 
-// The serial number of the device.  Useful for distinguishing amongst multiple connected devices.
+// `YES` if I am fully connected to my device.
+@property (nonatomic, readonly) BOOL isConnected;
+
+// I start disconnecting from the device on a background thread.  I notify my observers when I have finished disconnecting.
+- (void)disconnect;
+
+// I ask the device to start streaming distances to me.
+- (void)startStreaming;
+
+// I ask the device to stop streaming distances to me.
+- (void)stopStreaming;
+
+// `YES` if I expect the device to be sending me streaming data.
+@property (nonatomic, readonly) BOOL isStreaming;
+
+- (void)addObserver:(id<Lidar2DObserver>)observer;
+- (void)removeObserver:(id<Lidar2DObserver>)observer;
+
+// The serial number of the device.  Useful for distinguishing amongst multiple connected devices.  Only valid after `isConnected` becomes true.
 @property (nonatomic, readonly) NSString *serialNumber;
 
-// The number of distinct angles at which the device measures distance.  The measurements are equally-spaced around the arc of coverage.
+// The number of distinct angles at which the device measures distance.  The distances are equally-spaced around the arc of coverage.  Only valid after `isConnected` becomes true.
 @property (nonatomic, readonly) NSUInteger rayCount;
 
-// The size of the arc of coverage, in degrees.  The individual measurements are evenly spaced around this arc.  This is the difference between the first measurement angle and the last measurement angle.
+// The size of the arc of coverage, in degrees.  The individual distances are evenly spaced around this arc.  This is the difference between the first distance angle and the last distance angle.  Only valid after `coverageDegrees` becomes true.
 @property (nonatomic, readonly) double coverageDegrees;
-
-// I call the data snapshot block with a pointer to the distances detected by the device.  The `distances` array contains `rayCount` elements.
-typedef void (^Lidar2DDataSnapshotBlock)(uint32_t const *distances, BOOL *stop);
-
-// I ask the device to send me sensor data continuously.  Each time I receive a complete snapshot, I call `block`.  If `block` sets `*stop` to YES, I tell the device to stop sending me data and return.  If I encounter an error, I tell the device to stop sending me data and return.  Check `error` after I return.
-- (void)forEachStreamingDataSnapshot:(Lidar2DDataSnapshotBlock)block;
 
 @end
 
-@protocol Lidar2DProxy <NSObject>
+@protocol Lidar2DObserver
 
-// Execute `block` asynchronously on this device's private serial queue.
-- (void)performBlock:(void (^)(id<Lidar2D> device))block;
+@optional
 
-// Execute `block` synchronously on this device's private serial queue.
-- (void)performBlockAndWait:(void (^)(id<Lidar2D> device))block;
+// I encountered an error.
+- (void)lidar2d:(Lidar2D *)device didFailWithError:(NSError *)error;
+
+// I finished connecting to the physical device.
+- (void)lidar2dDidConnect:(Lidar2D *)device;
+
+// I finished disconnecting from the physical device.
+- (void)lidar2dDidDisconnect:(Lidar2D *)device;
+
+// I asked the device to start streaming distances.  You should expect to receive `lidar2D:didReceiveDistances:` messages.
+- (void)lidar2DWillBeginStreaming:(Lidar2D *)device;
+
+// I asked the device to stop streaming distances.  I will not send any more `lidar2D:didReceiveDistances:` messages until you send me `startStreaming` again.
+- (void)lidar2DDidStopStreaming:(Lidar2D *)device;
+
+// I received distance data from the device.  The `distances` array contains `device.rayCount` elements.
+- (void)lidar2d:(Lidar2D *)device didReceiveDistances:(Lidar2DDistance const *)distances;
 
 @end
 
