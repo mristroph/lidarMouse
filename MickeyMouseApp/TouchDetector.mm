d8d050431531bd02ab817db594f3a22bd2fa48f4
Rob Mayoff <git@rob.dqd.com>
2012-12-28 16:40:30 -0600

    switch to EWMATouchSelection

--- MickeyMouseApp/TouchDetector.mm
+++ MickeyMouseApp/TouchDetector.mm
@@ -40,7 +40,7 @@ using std::vector;
         touchCalibration_ = [[TouchCalibration alloc] init];
         touchCalibration_.delegate = self;
         touchCalibration_.thresholdCalibration = touchThresholdCalibration_;
-        if (YES) {
+        if (NO) {
             touchSelection_ = [[MiddleRayTouchSelection alloc] init];
         } else {
             touchSelection_ = [[EWMATouchSelection alloc] init];
